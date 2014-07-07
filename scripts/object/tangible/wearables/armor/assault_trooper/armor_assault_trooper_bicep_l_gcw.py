e93e6b06d84355fde43e8ba638503f6989c39368
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-07-07 13:13:26 -0400

    Removed Condition and Volume

--- scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_bicep_l_gcw.py
+++ scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_bicep_l_gcw.py
@@ -1,8 +1,6 @@
 import sys
 
 def setup(core, object):
-	object.setStringAttribute('condition', '40000/40000')
-	object.setIntAttribute('volume', 1)
 	object.setStringAttribute('armor_category', '@obj_attr_n:armor_assault')
 	object.setIntAttribute('cat_armor_standard_protection.kinetic', 6608)
 	object.setIntAttribute('cat_armor_standard_protection.energy', 4608)
--- scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_gloves_gcw.py
+++ scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_gloves_gcw.py
@@ -1,8 +1,6 @@
 import sys
 
 def setup(core, object):
-	object.setStringAttribute('condition', '40000/40000')
-	object.setIntAttribute('volume', 1)
 	object.setStringAttribute('required_faction', 'Imperial')
 	object.setStringAttribute('armor_category', '@obj_attr_n:armor_assault')
 	return	
