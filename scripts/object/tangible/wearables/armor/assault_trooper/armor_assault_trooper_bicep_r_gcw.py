a45ad0a8e3eaccbda3f0e9beac9a256bae42b4d9
Iosnowore Kun <pressinger.gene66@gmail.com>
2014-07-07 13:01:43 -0400

    Added Volume and Condition
    to Battle Worn Shock Trooper Armor

--- scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_bicep_r_gcw.py
+++ scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_bicep_r_gcw.py
@@ -1,12 +1,14 @@
 import sys
 
 def setup(core, object):
+	object.setStringAttribute('condition', '40000/40000')
+	object.setIntAttribute('volume', 1)
 	object.setStringAttribute('armor_category', '@obj_attr_n:armor_assault')
-	object.setStringAttribute('required_faction', 'Imperial')
 	object.setIntAttribute('cat_armor_standard_protection.kinetic', 6608)
 	object.setIntAttribute('cat_armor_standard_protection.energy', 4608)
 	object.setIntAttribute('cat_armor_special_protection.special_protection_type_heat', 5608)
 	object.setIntAttribute('cat_armor_special_protection.special_protection_type_cold', 5608)
 	object.setIntAttribute('cat_armor_special_protection.special_protection_type_acid', 5608)
 	object.setIntAttribute('cat_armor_special_protection.special_protection_type_electricity', 5608)
+	object.setStringAttribute('required_faction', 'Imperial')
 	return	
--- scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_utility_belt_gcw.py
+++ scripts/object/tangible/wearables/armor/assault_trooper/armor_assault_trooper_utility_belt_gcw.py
@@ -1,6 +1,8 @@
 import sys
 
 def setup(core, object):
+	object.setStringAttribute('condition', '100/100')
+	object.setIntAttribute('volume', 1)
 	object.setStringAttribute('required_level', '22')
 	object.setStringAttribute('required_faction', 'Imperial')
 	return	
